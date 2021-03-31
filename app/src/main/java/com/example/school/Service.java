1909cc416f49c2fd4c62304f558c9741842eb4f6
Gradskov Aleksej <gradskov2015@gmail.com>
2021-03-31 17:27:05 +0300

    lf

--- app/src/main/java/com/example/school/Service.java
+++ app/src/main/java/com/example/school/Service.java
@@ -1,14 +1,16 @@
 package com.example.school;
 
 public class Service {
-    private int id, duration, discount, mainImagePath;
-    private String title, description;
-    private double cost;
+    private final int duration;
+    private final int discount;
+    private final int mainImagePath;
+    private final String title;
+    private final String description;
+    private final double cost;
 
     public Service(int id, String title, double cost,
                    int duration, String description,
                    int discount, int mainImagePath) {
-        this.id = id;
         this.title = title;
         this.cost = cost;
         this.duration = duration;
@@ -21,59 +23,24 @@ public class Service {
         return cost + ((cost * discount) / 100);
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public int getDuration() {
         return duration;
-    }
-
-    public void setDuration(int duration) {
-        this.duration = duration;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 
     public int getDiscount() {
         return discount;
     }
 
-    public void setDiscount(int discount) {
-        this.discount = discount;
-    }
-
     public double getCost() {
         return cost;
-    }
-
-    public void setCost(double cost) {
-        this.cost = cost;
     }
 
     public int getMainImagePath() {
         return mainImagePath;
     }
 
-    public void setMainImagePath(int mainImagePath) {
-        this.mainImagePath = mainImagePath;
-    }
-
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
 }
--- app/src/main/java/com/example/school/ServiceAdapter.java
+++ app/src/main/java/com/example/school/ServiceAdapter.java
@@ -2,6 +2,7 @@ package com.example.school;
 
 import android.content.Context;
 import android.graphics.Paint;
+import android.icu.text.MessagePattern;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,48 +13,56 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
-    public class ServiceAdapter  extends RecyclerView.Adapter<ServiceAdapter.ViewHolder>{
+public class ServiceAdapter extends RecyclerView.Adapter<ServiceAdapter.ViewHolder> {
 
-        private final LayoutInflater inflater;
-        private final List<Service> services;
+    private final LayoutInflater inflater;
+    private final List<Service> services;
 
-        ServiceAdapter(Context context, List<Service> services) {
-            this.services = services;
-            this.inflater = LayoutInflater.from(context);
-        }
-        @Override
-        public ServiceAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = inflater.inflate(R.layout.list_item, parent, false);
-            return new ViewHolder(view);
-        }
+    ServiceAdapter(Context context, List<Service> services) {
+        this.services = services;
+        this.inflater = LayoutInflater.from(context);
+    }
 
-        @Override
-        public void onBindViewHolder(ServiceAdapter.ViewHolder holder, int position) {
-            Service service = services.get(position);
-            holder.serviceIMG.setImageResource(service.getMainImagePath());
-            holder.title.setText(service.getTitle());
+    @Override
+    public ServiceAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.list_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ServiceAdapter.ViewHolder holder, int position) {
+        Service service = services.get(position);
+        holder.serviceIMG.setImageResource(service.getMainImagePath());
+        holder.title.setText(service.getTitle());
+        if (service.getDiscount() != 0) {
+            holder.oldCost.setText(Double.toString(service.getOldCost()));
             holder.oldCost.setPaintFlags(holder.oldCost.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
-            holder.newCost.setText((int) service.getCost());
-            holder.duration.setText(service.getDuration());
-            holder.discount.setText(service.getDiscount());
+            holder.newCost.setText(Double.toString(service.getCost()));
+            holder.discount.setText("*"+Integer.toString(service.getDiscount())+"% скидка");
+        } else {
+            holder.newCost.setText(Double.toString(service.getCost()));
         }
+        holder.duration.setText(Integer.toString(service.getDuration()));
+    }
 
-        @Override
-        public int getItemCount() {
-            return services.size();
-        }
 
-        public static class ViewHolder extends RecyclerView.ViewHolder {
-            final ImageView serviceIMG;
-            final TextView title, oldCost, newCost, duration, discount;
-            ViewHolder(View view){
-                super(view);
-                serviceIMG = (ImageView)view.findViewById(R.id.serviceIMG);
-                title = (TextView) view.findViewById(R.id.textTitle);
-                oldCost = (TextView) view.findViewById(R.id.OldCostText);
-                newCost = (TextView) view.findViewById(R.id.newCostText);
-                duration = (TextView) view.findViewById(R.id.timeText);
-                discount = (TextView) view.findViewById(R.id.discountText);
-            }
+    @Override
+    public int getItemCount() {
+        return services.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        final ImageView serviceIMG;
+        final TextView title, oldCost, newCost, duration, discount;
+
+        ViewHolder(View view) {
+            super(view);
+            serviceIMG = (ImageView) view.findViewById(R.id.serviceIMG);
+            title = (TextView) view.findViewById(R.id.textTitle);
+            oldCost = (TextView) view.findViewById(R.id.OldCostText);
+            newCost = (TextView) view.findViewById(R.id.newCostText);
+            duration = (TextView) view.findViewById(R.id.timeText);
+            discount = (TextView) view.findViewById(R.id.discountText);
         }
+    }
 }
